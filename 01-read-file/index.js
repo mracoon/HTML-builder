69b27a62e65a19ce3baeae0cfddc154782d41e80
mracoon <nataliyakomarova777@gmail.com>
2022-10-30 00:18:47 +0300

    feat: add read from file - 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+
+const readStream = fs.createReadStream( path.join(__dirname, 'text.txt'), 'utf-8');
+
+let text = '';
+
+readStream.on('data', (chunk) => text += chunk);
+readStream.on('end', () => console.log(text));
