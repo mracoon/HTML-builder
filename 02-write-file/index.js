a06c6be22242ea11903e030091d18bb8739905a2
mracoon <nataliyakomarova777@gmail.com>
2022-10-30 15:22:43 +0300

    refactor: correct errors eslint for 02-write-file/index.js

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,34 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const { stdin: input, stdout: output } = require('process');
 
 const newFilePath = path.join(__dirname, 'text.txt');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-})
+});
 
 function writeMessageToFile(message) {
-  fs.appendFile(newFilePath, `${message}\n`, (err) => { if (err) throw err })
+  fs.appendFile(newFilePath, `${message}\n`, (err) => { if (err) throw err; });
 }
 
 function endSession() {
-  console.log('Goodbye!')
+  console.log('Goodbye!');
 }
 
-fs.writeFile(newFilePath, '', (err) => { if (err) throw err });
+fs.writeFile(newFilePath, '', (err) => { if (err) throw err; });
 
 rl.question('Enter message:\n', (message) => {
-  writeMessageToFile(message)
+  writeMessageToFile(message);
   rl.on('line', (mes) => {
     if (mes === 'exit') {
-      rl.close()
+      rl.close();
     } else {
-      writeMessageToFile(mes)
+      writeMessageToFile(mes);
     }
-  })
-  rl.on('close', endSession)
-})
+  });
+  rl.on('close', endSession);
+});
 
