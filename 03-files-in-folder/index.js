7ff284cf8c54bf12aa9929362bf06d0ee5c432cf
mracoon <98079971+mracoon@users.noreply.github.com>
2022-11-02 10:09:07 +0300

    fix: correct fileName & fileExt

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,8 +7,8 @@ fs.readdir(secretFolderPath, { withFileTypes: true }, (err, files) => {
   files.map(el => {
     if (el.isFile()) {
       const filePath = path.join(secretFolderPath, el.name);
-      const fileName = el.name.split('.')[0];
-      const fileExt = el.name.split('.')[1];
+      const fileExt = path.extname(el.name).slice(1);
+      const fileName = el.name.slice(0, -(fileExt.length + 1));
       let resultStr = fileName + ' - ' + fileExt + ' - ';
       fs.stat(filePath, (err, stats) => {
         resultStr += `${stats.size / 1024}kb`;
